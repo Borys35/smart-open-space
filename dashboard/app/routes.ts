288999e6cc570ffd92c20752f1a69e0452c550b0
Borys35 <boryskac10@gmail.com>
2026-04-17 16:48:20 +0200

    add login page

--- dashboard/app/routes.ts
+++ dashboard/app/routes.ts
@@ -1,13 +1,10 @@
 import { type RouteConfig, index, layout, prefix, route } from "@react-router/dev/routes"
 
 export default [
-    index("routes/home.tsx"),
     layout("./components/auth/public-only-route.tsx", [
         route("login", "routes/login.tsx"),
     ]),
     layout("./components/auth/protected-route.tsx", [
-        ...prefix("dashboard", [
-            index("routes/dashboard/index.tsx"),
-        ])
+        index("routes/dashboard/index.tsx"),
     ]),
 ] satisfies RouteConfig
