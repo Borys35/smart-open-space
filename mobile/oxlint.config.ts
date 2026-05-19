785f35523894d9fd07be9a00d555d90ef426d1d8
ssobkowski <ssssobek@gmail.com>
2026-05-19 23:03:28 +0200

    chore(mobile): update oxc configs

--- mobile/oxlint.config.ts
+++ mobile/oxlint.config.ts
@@ -3,4 +3,9 @@ import native from "oxlint-config-universe/native";
 
 export default defineConfig({
   extends: [native],
+  plugins: ["react"],
+  rules: {
+    "react/exhaustive-deps": "error",
+    curly: ["warn", "multi-line"],
+  },
 });
