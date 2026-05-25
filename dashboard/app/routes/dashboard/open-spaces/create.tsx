057d7dfe1d3fd6edce6305a50393d5a196635983
Borys35 <boryskac10@gmail.com>
2026-05-25 20:42:21 +0200

    dashboard: fix forms in mobile version

--- dashboard/app/routes/dashboard/open-spaces/create.tsx
+++ dashboard/app/routes/dashboard/open-spaces/create.tsx
@@ -9,7 +9,7 @@ export default function OpenSpaceCreate() {
     return (
         <div className="flex flex-col h-full w-full p-4 md:p-6 lg:p-8">
             <h1 className="text-2xl font-bold mb-6">Create Open Space</h1>
-            <div className="w-lg self-center pt-8">
+            <div className="w-full lg:w-lg self-center pt-8">
                 <CreateOpenSpaceForm />
             </div>
         </div>
--- dashboard/app/routes/dashboard/open-spaces/settings.tsx
+++ dashboard/app/routes/dashboard/open-spaces/settings.tsx
@@ -10,7 +10,7 @@ export default function OpenSpaceCreate() {
     return (
         <div className="flex flex-col h-full w-full p-4 md:p-6 lg:p-8">
             <h1 className="text-2xl font-bold mb-6">Open Space Settings</h1>
-            <div className="w-lg self-center pt-8">
+            <div className="w-full lg:w-lg self-center pt-8">
                 <OpenSpaceSettingsForm />
             </div>
         </div>
